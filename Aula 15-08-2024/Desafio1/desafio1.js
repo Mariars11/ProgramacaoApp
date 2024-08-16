c429c922cd79e30e8ff9b86b24a48051cf7c552a
Maria Clara <maria.clara@bcb.gov.br>
2024-08-16 15:33:40 -0300

    Adicionando melhorias no desafio 1

--- Aula 15-08-2024/Desafio1/desafio1.js
+++ Aula 15-08-2024/Desafio1/desafio1.js
@@ -1,30 +1,37 @@
 // 1 - crie um programa que sorteei numeros da loteria, aleatoria, 
 //conforme o jogo escolhido. Não podem ter o mesmo numero, sem repeticao.
 
+//id jogo para acrescentar os jogos criados
+let jogo = document.getElementById("jogo");
+
+//Limpa o html
+function limpar(){
+    jogo.innerHTML = "";
+}
 
 function escolherLoteria(){
     //alert("Entrei!");
-    var e = document.getElementById("opcoesLoteria");
-    var value = e.value;
-    var text = e.options[e.selectedIndex].text;
+    var opcoes = document.getElementById("opcoesLoteria");
+    //O texto do jogo selecionado (mega sena; quina; lotofacil)
+    var text = opcoes.value.options[e.selectedIndex].text;
     let listaJogo = [];
     if(text != null && text != ""){
         switch (text) {
             case "Mega Sena":{
-                //console.log("Entrei")
+                // //console.log("Entrei")
                 let count = 0;
                 let maxNumbers = 6;
                 
                 while(count < maxNumbers){
                     let numberRandom = Math.floor(Math.random() * 100);;
-                    console.log(numberRandom);
-                    console.log(listaJogo.indexOf(numberRandom));
+                    // console.log(numberRandom);
+                    // console.log(listaJogo.indexOf(numberRandom));
                     if(listaJogo.indexOf(numberRandom) == -1 && numberRandom > 0 && numberRandom < 61){
-                        console.log("entrei");
+                        // console.log("entrei");
                         listaJogo.push(numberRandom);
                         count++;
                     }
-                    console.log(count);
+                    // console.log(count);
                 }
                 break;
             }
@@ -34,32 +41,32 @@ function escolherLoteria(){
                 
                 while(count < maxNumbers){
                     let numberRandom = Math.floor(Math.random() * 100);;
-                    console.log(numberRandom);
-                    console.log(listaJogo.indexOf(numberRandom));
+                    // console.log(numberRandom);
+                    // console.log(listaJogo.indexOf(numberRandom));
                     if(listaJogo.indexOf(numberRandom) == -1 && numberRandom > 0 && numberRandom < 81){
-                        console.log("entrei");
+                        // console.log("entrei");
                         listaJogo.push(numberRandom);
                         count++;
                     }
-                    console.log(count);
+                    // console.log(count);
                 }
                 break;
             }   
             case "Lotofácil":{
-                console.log("Entrei")
+                // console.log("Entrei")
                 let count = 0;
                 let maxNumbers = 15;
                 
                 while(count < maxNumbers){
                     let numberRandom = Math.floor(Math.random() * 100);;
-                    console.log(numberRandom);
-                    console.log(listaJogo.indexOf(numberRandom));
+                    // console.log(numberRandom);
+                    // console.log(listaJogo.indexOf(numberRandom));
                     if(listaJogo.indexOf(numberRandom) == -1 && numberRandom > 0 && numberRandom < 26){
-                        console.log("entrei");
+                        // console.log("entrei");
                         listaJogo.push(numberRandom);
                         count++;
                     }
-                    console.log(count);
+                    // console.log(count);
                 }
                 break;
             }    
@@ -67,10 +74,16 @@ function escolherLoteria(){
                 alert("Opção invalida");
                 break;
         }
-        document.write(`<h1>${text}</h1>`)
+        let h1 = document.createElement("h1");
+        h1.innerText = text;
+        jogo.appendChild(h1);
+        let concatNumeros = "";
+        let p = document.createElement("p");
         listaJogo.forEach(item => {
-            document.write(`<p>${item} </p>`);
-            
+            //document.write();
+            p.innerText += `${item} - `;
         });
+        // console.log(concatNumeros);
+        jogo.appendChild(p);
     }
 }
